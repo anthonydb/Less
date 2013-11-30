95b080be8d44f04031c3746fcbe1173b35c225fb
Anthony DeBarros <adebarros@gmail.com>
2013-11-30 17:20:16 -0500

    Add my info to the footer.

--- index.php
+++ index.php
@@ -221,13 +221,23 @@
 <footer class="site-footer" role="contentinfo">
 	<div class="site-info container">
 		<?php do_action( 'break_credits' ); ?>
-		<a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress</a>
-		<span class="sep"> and </span>
-		<a href="http://lessmade.com/themes/less" rel="theme">LESS</a> by <a href="http://jarederickson.com" rel="designer">Jared Erickson</a>
+		<p>Copyright MMXIV Anthony DeBarros.<br />
+		I'm <a href="https://github.com/anthonydb/Less">hacking</a> on the <a href="http://lessmade.com/themes/less" rel="theme">LESS</a> theme by <a href="http://jarederickson.com" rel="designer">Jared Erickson</a>. 
+		Hosting is by <a href="http://www.webfaction.com/signup?affiliate=adebarros" >WebFaction</a>. </p>
 	</div><!-- .site-info -->
 </footer><!-- #colophon .site-footer -->
 
 <?php wp_footer(); ?>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-11819184-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
 
 </body>
 </html>
