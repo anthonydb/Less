3774a90510b5acb9587ba818e2d150ecc32c1a83
Anthony DeBarros <adebarros@gmail.com>
2013-12-01 17:30:56 -0500

    Hacking around Google.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
-<title><?php bloginfo('name'); ?> | <?php if( is_home() ) : echo bloginfo( 'description' ); endif; ?><?php wp_title( '', true ); ?></title>
+<title><?php bloginfo('name'); ?> &#58;&#58; <?php if( is_home() ) : echo bloginfo( 'description' ); endif; ?><?php wp_title( '', true ); ?></title>
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
